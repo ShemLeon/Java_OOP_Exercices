5836db14e8850be7cca35634868994bb1e24d46b
ShemLeon <shemiakinleonid@gmail.com>
2025-04-24 14:20:20 +0300

    Interface Iron

--- src/Exercices_OOP/_8_Interface/t1_Product/Iron.java
+++ src/Exercices_OOP/_8_Interface/t1_Product/Iron.java
@@ -31,6 +31,41 @@ public class Iron extends Product implements PayAble {
         this.auto = true;
     }
 
+    public Iron(){
+        super();
+        this.nItems = 0;
+        this.price = 0;
+        this.auto = false;
+    }
+    public String toString(){
+        return super.toString()+"num of Iteams: "+this.nItems+
+                "price :"+this.price+ "Auto"+this.auto;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean ans = false;
+        if (obj instanceof Iron){
+            if(super.equals(obj)){
+                Iron ir = (Iron)obj;
+                if ((this.nItems==ir.nItems)&&(this.price==ir.price))
+                    ans = true;
+            }
+        }
+        return ans;
+    }
+
+    public void printPrice()
+    {
+        System.out.println(" my Item is an Iron : " +
+                super.toString()+ "price: "+this.price);
+    }
+    public boolean discount() {
+        return ((this.nItems > 100) && (this.price > 20));
+    }
+    public double stock(){
+        return nItems * price;
+    }
 
 
     public static void main(String[] args) {
--- src/Exercices_OOP/_8_Interface/t1_Product/PayAble.java
+++ src/Exercices_OOP/_8_Interface/t1_Product/PayAble.java
@@ -1,5 +1,9 @@
 package Exercices_OOP._8_Interface.t1_Product;
 
 public interface PayAble {
-
+    public void printPrice();
+    public boolean discount();
+    public double stock();
+    public boolean equals();
+    public String toString();
 }
