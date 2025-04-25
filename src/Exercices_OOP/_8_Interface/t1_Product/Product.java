04ae1185fd3ed14102996b59f61d45c12d2e9cf4
Leonid <elieyzer1@gmail.com>
2025-04-25 11:48:31 +0300

    Eng localization 8_interface

--- src/Exercices_OOP/_8_Interface/t1_Product/Product.java
+++ src/Exercices_OOP/_8_Interface/t1_Product/Product.java
@@ -10,25 +10,21 @@ public class Product {
     protected String model; // model
     private int prodNum; // product number
     public static int count = 0;
-
     public Product(String mf, String model) {
         this.mf = mf+"";
         this.model = model+"";
         count++;
         prodNum=count;
     }
-
     public Product( ) {
         this.mf = "LG";
         this.model = "220055";
         count++;
         prodNum = count;
     }
-
     public String toString(){
         return "Product Num: "+this.prodNum+this.mf+this.model;
     }
-
     public boolean equals(Object obj){
         boolean ans = false;
         if (obj instanceof Product){
@@ -38,5 +34,4 @@ public class Product {
         }
         return ans;
     }
-
 }
