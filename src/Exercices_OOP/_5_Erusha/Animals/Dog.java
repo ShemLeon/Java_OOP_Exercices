9492f14d10abe90f8c77ed5acbff3c44baa4db87
ShemLeon <shemiakinleonid@gmail.com>
2025-06-21 16:01:58 +0300

    Erusha Dog

--- src/Exercices_OOP/_5_Erusha/Animals/Dog.java
+++ src/Exercices_OOP/_5_Erusha/Animals/Dog.java
@@ -6,10 +6,17 @@ public class Dog extends Mammal{
         super(name, gender, milk);
         this.bonesToEat =bonesToEat;
     }
+    public void hideBones(int bonesToHide){
+        System.out.println("Hide bones: "+bonesToHide);
+    }
+    @Override
+    public String toString(){
+        return super.toString()+", bones to eat: "+bonesToEat;
+    }
 
     public static void main(String[] args) {
-        //
-        System.out.println("Shalom!");
-        //
+       Dog d1 = new Dog("Sobaka", "F", 1, 3);
+       System.out.println(d1);
+       d1.hideBones(2);
     }
 }
