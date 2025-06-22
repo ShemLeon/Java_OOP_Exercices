596f174974a175d9fd3c1dfafdf39bba9efb268c
ShemLeon <shemiakinleonid@gmail.com>
2025-06-22 03:33:06 +0300

    final class

---
+++ src/Exercices_OOP/_0_Start/Carr.java
@@ -0,0 +1,19 @@
+package Exercices_OOP._0_Start;// import unit4.collectionsLib.Node;
+
+public class Carr {
+    public String make;
+    public int year;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Carr carr1 = new Carr();
+        carr1.make = "Toyota";
+        carr1.year = 2020;
+        System.out.println("Car Make: " + carr1.make + ", Year: " + carr1.year);
+// 
+    }
+}
+
+
+
