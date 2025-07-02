5e2ee277613dbfb4889bb9de033eac760c5f581e
ShemLeon <shemiakinleonid@gmail.com>
2025-07-02 05:20:50 +0300

    Enum start

---
+++ src/Exercices_OOP/_9_Enum/dugma_enum.java
@@ -0,0 +1,9 @@
+package Exercices_OOP._9_Enum;// import unit4.collectionsLib.Node;
+
+public class dugma_enum {
+    public static void main(String[] args) {
+        //
+        System.out.println("Shalom!");
+        //
+    }
+}
